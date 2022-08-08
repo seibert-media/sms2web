f8e0b355c95be7e8e843ba1002e32301670d4794
mwiegand <mwiegand@seibert-media.net>
2022-08-08 18:14:06 +0200

    refactor and fix

--- sms2web/__init__.py
+++ sms2web/__init__.py
@@ -70,25 +70,28 @@ def home():
 @app.route('/sms77', methods=['POST'])
 def sms77():
     # {"webhook_event":"sms_mo","webhook_timestamp":"2021-05-04T13:15:12+02:00","data":{"id":800342,"sender":"491702607871","time":1620126912,"text":"Test","system":"4915735990598"}}
-    sms_data = request.get_json()['data']
-    sms_data['timestamp'] = int(sms_data['timestamp'])
-    sms_data['received_at'] = int(time())
+    sms = {
+        'sender': request.get_json()['data']['sender'],
+        'message': request.get_json()['data']['text'],
+        'timestamp': int(request.get_json()['data']['time']),
+        'received_at': int(time()),
+    }
     insert(
         '''
             INSERT INTO sms (sender, message, timestamp, received_at)
             VALUES (:sender, :message, :timestamp, :received_at)
         ''',
-        sender=sms_data['sender'],
-        message=sms_data['text'],
-        timestamp=sms_data['time'],
-        received_at=sms_data['received_at']
+        sender=sms['sender'],
+        message=sms['message'],
+        timestamp=sms['timestamp'],
+        received_at=sms['received_at']
     )
 
     post_receive_hook_path = environ.get('SMS2WEB_POST_RECEIVE_HOOK_PATH', None)
     if post_receive_hook_path:
-        print('CALLLING HOOK...', json.dumps(sms_data))
+        print('CALLLING HOOK...', json.dumps(sms))
         try:
-            print('HOOK OUTPUT:', check_output([post_receive_hook_path, json.dumps(sms_data)]), file=sys.stderr)
+            print('HOOK OUTPUT:', check_output([post_receive_hook_path, json.dumps(sms)]), file=sys.stderr)
         except CalledProcessError as e:
             print('HOOK ERROR:', e.output, file=sys.stderr)
 
